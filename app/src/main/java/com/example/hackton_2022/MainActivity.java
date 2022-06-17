1801482f6e7803b903d34c6ec6bb610956e44ce2
Eliud Ayala <eliud.ayala20@hotmail.com>
2022-06-17 14:32:06 -0500

    Primer commit

--- app/src/main/java/com/example/hackton_2022/MainActivity.java
+++ app/src/main/java/com/example/hackton_2022/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.hackton_2022;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btn_enviar = findViewById(R.id.btn_enviar);
+        btn_enviar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent (v.getContext(), MapsActivity.class);
+                startActivityForResult(intent, 0);
+            }
+        });
     }
 }
