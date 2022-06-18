bed50a78c16f201ca66b66d436e3671253d3192b
Eliud Ayala <eliud.ayala20@hotmail.com>
2022-06-17 22:58:31 -0500

    Se agregan dos actividades y se hace la base para la descripcion

--- app/src/main/java/com/example/hackton_2022/Descripcion.java
+++ app/src/main/java/com/example/hackton_2022/Descripcion.java
@@ -2,21 +2,20 @@ package com.example.hackton_2022;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
 import android.content.Intent;
+import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-
-public class MainActivity extends AppCompatActivity {
+public class Descripcion extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_descripcion);
 
-        Button btn_enviar = findViewById(R.id.btn_enviar);
-        btn_enviar.setOnClickListener(new View.OnClickListener() {
+        Button btn_reg = findViewById(R.id.btn_regr);
+        btn_reg.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent (v.getContext(), Prueba_Scroll.class);
--- app/src/main/java/com/example/hackton_2022/Prueba_Scroll.java
+++ app/src/main/java/com/example/hackton_2022/Prueba_Scroll.java
@@ -2,26 +2,27 @@ package com.example.hackton_2022;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
 import android.content.Intent;
+import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-
-public class MainActivity extends AppCompatActivity {
+public class Prueba_Scroll extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_prueba_scroll);
 
-        Button btn_enviar = findViewById(R.id.btn_enviar);
-        btn_enviar.setOnClickListener(new View.OnClickListener() {
+        Button btn_mas = findViewById(R.id.btn_mas);
+        btn_mas.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent (v.getContext(), Prueba_Scroll.class);
+                Intent intent = new Intent (v.getContext(), Descripcion.class);
                 startActivityForResult(intent, 0);
             }
         });
     }
+
+
 }
